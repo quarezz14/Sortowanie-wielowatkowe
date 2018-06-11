93dcc9c704b05033e294c596eea64866776a14ca
quarezz14 <39925561+quarezz14@users.noreply.github.com>
2018-06-11 15:33:24 +0200

    usunięcie zbędnych komentarzy

--- Main.java
+++ Main.java
@@ -3,8 +3,6 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		//test
 		
 		Ramka okienko = new Ramka();
 		okienko.repaint();
